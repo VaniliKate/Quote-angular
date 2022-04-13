a215047e735b67a4241dc2a5a1552660b0b68f2d
Vanili <katevanili98@gmail.com>
2022-04-13 21:58:11 +0300

    Implemented two way data binding using ngModel

--- Documents/Angular-projects/Git-app/src/app/github-app/github-app.component.ts
+++ Documents/Angular-projects/Git-app/src/app/github-app/github-app.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GithubAppComponent implements OnInit {
 
+  public githubUserQuery!: string;
+
+
   constructor() { }
 
   ngOnInit(): void {
